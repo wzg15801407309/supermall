80b2f648bc74737960753ba0386039548c06f1eb
wzg15801407309 <1051084301@qq.com>
2020-02-23 14:43:24 +0800

    navbar

---
+++ src/components/common/navbar/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="nav-bar">
+    <div class="nav-left">
+      <slot name="nav-left"></slot>
+    </div>
+    <div class="nav-center">
+      <slot name="nav-center"></slot>
+    </div>
+    <div class="nav-right">
+      <slot name="nav-right"></slot>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: "navbar"
+  };
+
+</script>
+<style lang="less" scoped>
+  .nav-bar {
+    display: flex;
+    /* 使用这个设置高度，他可以直接撑起高度切文字还能居中显示 */
+    line-height: 44px;
+    height: 44px;
+    box-shadow: 0 1px 1px rgba(100, 100, 100, 0.05);
+    text-align: center;
+
+    .nav-left,
+    .nav-right {
+      width: 60px;
+    }
+
+    .nav-center {
+      flex: 1;
+    }
+  }
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,3 +1,37 @@
 <template>
-    <div>home</div>
+  <div id="home">
+    <navBar class="home-nav">
+      <div slot="nav-center">购物街</div>
+    </navBar>
+  </div>
 </template>
+
+<script>
+import navBar from "@/components/common/navbar";
+export default {
+  name: "home",
+  components: { navBar },
+  props: {},
+  data() {
+    return {};
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+</script>
+<style lang="less" scoped>
+#home {
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+}
+</style>
